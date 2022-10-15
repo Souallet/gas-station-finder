23f396dac177eb10f3891ca90a21d204cd41c63e
Sébastien Ouallet <sebastien.ouallet@gmail.com>
2022-10-15 18:41:13 +0200

    Project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div className="grid place-content-center h-screen">
+            <h1 className="text-3xl">Gas Station</h1>
+        </div>
+    );
+}
+
+export default App;
