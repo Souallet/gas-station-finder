98114f1278b3e6cb484cfbc3be86cce84fae2b72
Sébastien Ouallet <sebastien.ouallet@gmail.com>
2022-10-18 11:45:42 +0200

    Reworked hero

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,12 @@ import React from 'react';
 
 import { StationsProvider } from './contexts/StationsContext';
 import Router from './config/router';
+import Hero from './components/header/Hero';
 
 function App() {
     return (
         <StationsProvider>
-            <header className="container mx-auto px-12 py-4">
-                <h1 className="text-3xl">Gas Station</h1>
-            </header>
+            <Hero />
             <Router />
         </StationsProvider>
     );
---
+++ src/components/header/Hero.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+function Hero() {
+    return (
+        <header className="relative bg-indigo-500 py-20 mb-4 ">
+            <div className="flex justify-center w-full">
+                <div className="lg:w-6/12 text-center">
+                    <h1 className="text-white font-semibold text-5xl">Gas Station</h1>
+                    <p className="mt-12 text-lg text-white">
+                        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Itaque ducimus
+                        quisquam porro eum enim maiores quo possimus numquam, veniam temporibus ab
+                        praesentium explicabo nam illum eligendi! Assumenda eligendi laborum nulla!
+                    </p>
+                </div>
+            </div>
+        </header>
+    );
+}
+
+export default Hero;
